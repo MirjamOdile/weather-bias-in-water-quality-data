8f3bb302dcbf6a2c9915a3f27e14f9717dee4298
Mikkel Bue Lykkegaard <m.lykkegaard@exeter.ac.uk>
2021-09-04 13:02:33 +0100

    Added Bayesian test and bias-corrected section

--- Code/2_2_ImpactModels_Models.R
+++ Code/2_2_ImpactModels_Models.R
@@ -19,6 +19,7 @@ library(fritools) # weighted variance
 # Specify root as path to 'Data Cleaned' folder containing the lake data folders 
 # 'Manual' and 'WeatherStation'
 root <- '/home/mirjam/GitHub/weather-bias-in-water-quality-data/Data/Data Cleaned/'
+root <- '/home/mikkel/OneDrive/Projects/WeatherBiasArticle/Data/Data Cleaned'
 
 # Set the working directory
 
@@ -178,12 +179,12 @@ rm(matched.automatic.agg, matched.manual.agg)
 ggplot(paired, aes(Temp.manual.mean.weighted, 
                            Temp.automatic.mean, 
                            color = as.factor(Depth))) + 
-  geom_point() + 
+  geom_point(size=1) + 
   geom_errorbar(aes(ymin = Temp.automatic.mean - sqrt(Temp.automatic.var),
                     ymax = Temp.automatic.mean + sqrt(Temp.automatic.var)), 
                     alpha = 0.5) +
   geom_abline(slope = 1, intercept = 0, color = 'midnightblue') +
-  labs(x = 'Manual: weighted daily mean temperature (°C)', 
+  labs(x = 'Manual: weighted mean temperature (°C)', 
        y = 'Automatic: daily mean temperature (°C)', color = 'Depth (m)') +
   scale_colour_viridis_d(option = 'plasma', end = 0.8, direction = -1)
 # >> Low variance (y-axis error bars) shows that daily aggregation of the 
@@ -204,6 +205,77 @@ ggplot(paired, aes(as.factor(Depth),
   labs(x = 'Depth (m)', color = 'Depth (m)', fill = 'Depth (m)',
        y = 'Difference manual vs. automatic daily mean temperature (°C)')
 # >> Confirms bias at deeper depths (9-19m).
+
+# Rotated and annotated version
+textpos <- -4.25
+arrowpos <- -3.75
+
+ggplot(paired, aes(reorder(as.factor(Depth), desc(as.factor(Depth))), 
+                   Temp.manual.mean.weighted-Temp.automatic.mean, 
+                   color = as.factor(Depth),
+                   fill = as.factor(Depth))) +
+  geom_hline(yintercept=0, linetype='dotted') +
+  geom_violin(size = .7) +
+  geom_point(shape = 'o', size=2) +
+  scale_color_viridis_d(option = 'plasma', end = 0.8, direction = -1) +
+  scale_fill_viridis_d(option = 'plasma', end = 0.8, direction = -1,
+                       alpha = 0.4) +
+  labs(x = 'Depth (m)', color = 'Depth (m)', fill = 'Depth (m)',
+       y = 'Manual Temperature Bias (°C)') + 
+  ylim(-5,5) +
+  annotate("text", x = 8.5, y = textpos, angle=90, label = "Epilimnion") +
+  geom_segment(aes(x = 8.5, y = arrowpos, xend = 10, yend = arrowpos),
+               arrow = arrow(length = unit(0.5, "cm"), angle=45), 
+               size=0.5, color='gray20', lineend = 'round', linejoin = 'bevel') +
+  geom_segment(aes(x = 8.5, y = arrowpos, xend = 7, yend = arrowpos),
+               arrow = arrow(length = unit(0.5, "cm"), angle=45), 
+               size=0.5, color='gray20', lineend = 'round', linejoin = 'bevel') +
+  annotate("text", x = 3.5, y = textpos, angle=90, label = "Thermocline and Hypolimnion") +
+  geom_segment(aes(x = 3.55, y = arrowpos, xend = 6, yend = arrowpos),
+               arrow = arrow(length = unit(0.5, "cm"), angle=45), 
+               size=0.5, color='gray20', lineend = 'round', linejoin = 'bevel') +
+  geom_segment(aes(x = 3.55, y = arrowpos, xend = 1, yend = arrowpos),
+               arrow = arrow(length = unit(0.5, "cm"), angle=45), 
+               size=0.5, color='gray20', lineend = 'round', linejoin = 'bevel') +
+  coord_flip() +
+  theme(legend.position='none',
+        axis.text=element_text(size=12),
+        axis.title=element_text(size=14))
+
+# Histograms
+hist1 <- ggplot(paired) + 
+  geom_histogram(aes(x=Temp.manual.mean.weighted, y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(aes(x=Temp.manual.mean.weighted), color='orangered', size=1, bw=1) +
+  geom_histogram(aes(x=Temp.automatic.mean, y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(aes(x=Temp.automatic.mean), color='steelblue1', size=1, bw=1) +
+  scale_y_continuous(breaks=c(0.1, 0.2)) +
+  ggtitle('All measurement depths') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = element_blank(),
+       y = 'Density')
+
+hist2 <- ggplot(filter(paired, Depth<=7)) + 
+  geom_histogram(aes(x=Temp.manual.mean.weighted, y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(aes(x=Temp.manual.mean.weighted), color='orangered', size=1, bw=1) +
+  geom_histogram(aes(x=Temp.automatic.mean, y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(aes(x=Temp.automatic.mean), color='steelblue1', size=1, bw=1) +
+  scale_y_continuous(breaks=c(0.1, 0.2)) +
+  ggtitle('Measurement Depths 1-7m') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = element_blank(),
+       y = 'Density')
+
+hist3 <- ggplot(filter(paired, Depth>7)) + 
+  geom_histogram(aes(x=Temp.manual.mean.weighted, y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(aes(x=Temp.manual.mean.weighted), color='orangered', size=1, bw=1) +
+  geom_histogram(aes(x=Temp.automatic.mean, y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(aes(x=Temp.automatic.mean), color='steelblue1', size=1, bw=1) +
+  ggtitle('Measurement Depths 9-19m') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = 'Temperature (°C)',
+       y = 'Density')
+
+grid.arrange(hist1, hist2, hist3)
 
 #--------------------------#
 #  Run the paired t-tests  #
@@ -227,13 +299,96 @@ t.test(paired$Temp.manual.mean.weighted[paired$Depth>7],
 
 # >> We find a statistically significant difference in means at the deeper 
 # depths 9-19m, which we theorise is due to the rapid temperature changes in 
-# the thermocline in combination with sensor lag. 
+# the thermocline in combination with sensor lag.
+
+# Bayesian estimation for two groups
+library(BayesFactor)
+library(bayestestR)
+
+# All depths
+fit_all <- ttestBF(paired$Temp.manual.mean.weighted, 
+                   paired$Temp.automatic.mean, 
+                   paired = T, posterior=T, iterations=10000)
+summary(fit_all)
+hdi(fit_all)
+p_rope(fit_all)
+plot(fit_all)
+
+# Lower depths
+fit_lo <- ttestBF(paired$Temp.manual.mean.weighted[paired$Depth<=7], 
+                  paired$Temp.automatic.mean[paired$Depth<=7], 
+                  paired = T, posterior=T, iterations=10000)
+summary(fit_lo)
+hdi(fit_lo)
+p_rope(fit_lo)
+plot(fit_lo)
+
+# Higher depths
+fit_hi <- ttestBF(paired$Temp.manual.mean.weighted[paired$Depth>7], 
+                  paired$Temp.automatic.mean[paired$Depth>7], 
+                  paired = T, posterior=T, iterations=10000)
+summary(fit_hi)
+hdi(fit_hi)
+p_rope(fit_hi)
+plot(fit_hi)
+
+# Jensen-Shannon divergence
+library(philentropy)
+
+bins = seq(4, 30, 1)
+hist_manual_all = hist(paired$Temp.manual.mean.weighted, breaks=bins, plot=F)
+hist_automatic_all = hist(paired$Temp.automatic.mean, breaks=bins, plot=F)
+JSD(rbind(hist_manual_all$density, hist_automatic_all$density))
+
+hist_manual_lo = hist(paired$Temp.manual.mean.weighted[paired$Depth<=7], breaks=bins, plot=F)
+hist_automatic_lo = hist(paired$Temp.automatic.mean[paired$Depth<=7], breaks=bins, plot=F)
+JSD(rbind(hist_manual_lo$density, hist_automatic_lo$density))
+
+hist_manual_hi = hist(paired$Temp.manual.mean.weighted[paired$Depth>7], breaks=bins, plot=F)
+hist_automatic_hi = hist(paired$Temp.automatic.mean[paired$Depth>7], breaks=bins, plot=F)
+JSD(rbind(hist_manual_hi$density, hist_automatic_hi$density))
 
 
 ##################################################################
 ###  Two-sample t-test: are the means of the population of the ###
 ###          automatic and manual samples the same?            ###
 ##################################################################
+
+# Histogram plots of the two-sample data
+hist1 <- ggplot(manual, aes(Temp)) + 
+  geom_histogram(aes(y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(color='orangered', size=1, bw=1) +
+  geom_histogram(data=automatic, aes(y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(data=automatic, color='steelblue1', size=1, bw=1) +
+  scale_y_continuous(breaks=c(0.1, 0.2)) +
+  ggtitle('All measurement depths') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = element_blank(),
+       y = 'Density')
+
+hist2 <- ggplot(filter(manual, Depth<=8), aes(Temp)) + 
+  geom_histogram(aes(y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(color='orangered', size=1, bw=1) +
+  geom_histogram(data=filter(automatic, Depth<=7), aes(y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(data=filter(automatic, Depth<=7), color='steelblue1', size=1, bw=1) +
+  scale_y_continuous(breaks=c(0.1, 0.2)) +
+  ggtitle('Measurement Depths 0-8m') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = element_blank(),
+       y = 'Density')
+
+hist3 <- ggplot(filter(manual, Depth>8),aes(Temp)) + 
+  geom_histogram(aes(y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(color='orangered', size=1, bw=1) +
+  geom_histogram(data=filter(automatic, Depth>7), aes(y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(data=filter(automatic, Depth>7), color='steelblue1', size=1, bw=1) +
+  ggtitle('Measurement Depths 8-20m') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = 'Temperature (°C)',
+       y = 'Density')
+
+
+grid.arrange(hist1, hist2, hist3)
 
 #-------------------#
 #  Run the t-tests  #
@@ -253,6 +408,156 @@ t.test(manual$Temp[manual$Depth <= 7], automatic$Temp[automatic$Depth <= 7])
 t.test(manual$Temp[manual$Depth > 7], automatic$Temp[automatic$Depth > 7])
        # alternative = 'greater')
 # >> Statistically significant difference in means.
+
+# Bayesian estimation for two groups
+fit_all <- ttestBF(manual$Temp, automatic$Temp,
+                   posterior=T, iterations=10000)
+summary(fit_all)
+hdi(fit_all)
+p_rope(fit_all)
+plot(fit_all)
+
+# 1-7m
+fit_lo <- ttestBF(manual$Temp[manual$Depth <= 7], 
+                  automatic$Temp[automatic$Depth <= 7],
+                  posterior=T, iterations=10000)
+summary(fit_lo)
+hdi(fit_lo)
+p_rope(fit_lo)
+plot(fit_lo)
+
+# 9-19m
+fit_hi <- ttestBF(manual$Temp[manual$Depth > 7], 
+                  automatic$Temp[automatic$Depth > 7], 
+                  posterior=T, iterations=10000)
+summary(fit_hi)
+hdi(fit_hi)
+p_rope(fit_hi)
+plot(fit_hi)
+
+# Jensen-Shannon divergence
+hist_manual_all = hist(manual$Temp, breaks=bins, plot=F)
+hist_automatic_all = hist(automatic$Temp, breaks=bins, plot=F)
+JSD(rbind(hist_manual_all$density, hist_automatic_all$density))
+
+hist_automatic_lo = hist(automatic$Temp[automatic$Depth<=7], breaks=bins, plot=F)
+hist_manual_lo = hist(manual$Temp[manual$Depth<=7], breaks=bins, plot=F)
+JSD(rbind(hist_manual_lo$density, hist_automatic_lo$density))
+
+hist_automatic_hi = hist(automatic$Temp[automatic$Depth>7], breaks=bins, plot=F)
+hist_manual_hi = hist(manual$Temp[manual$Depth>7], breaks=bins, plot=F)
+JSD(rbind(hist_manual_hi$density, hist_automatic_hi$density))
+
+##################################################################
+###  Two-sample t-test: Bias-corrected data                    ###
+##################################################################
+
+# Find the differences for each depth.
+differences <- paired %>% group_by(Depth) %>%
+  summarise(Diff = t.test(Temp.manual.mean.weighted, Temp.automatic.mean, paired = T)$estimate[[1]],
+            CI_lo = t.test(Temp.manual.mean.weighted, Temp.automatic.mean, paired = T)$conf.int[1],
+            CI_hi = t.test(Temp.manual.mean.weighted,  Temp.automatic.mean, paired = T)$conf.int[2],
+            t = t.test(Temp.manual.mean.weighted,  Temp.automatic.mean, paired = T)$statistic[[1]],
+            df = t.test(Temp.manual.mean.weighted,  Temp.automatic.mean, paired = T)$parameter[[1]],
+            p = t.test(Temp.manual.mean.weighted, Temp.automatic.mean, paired = T)$p.value[[1]])
+
+#library(stargazer)
+#stargazer(as.data.frame(differences), summary=F, rownames=F)
+
+# Correct using the computed differences.
+manual <- manual %>%
+  rowwise() %>%
+  mutate(Temp_corrected = Temp-differences$Diff[differences$Depth==Depth])
+
+# Plot the corrected manual measurements.
+hist1 <- ggplot(manual, aes(Temp_corrected)) + 
+  geom_histogram(aes(y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(color='orangered', size=1, bw=1) +
+  geom_histogram(data=automatic, aes(x=Temp, y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(data=automatic, aes(x=Temp), color='steelblue1', size=1, bw=1) +
+  scale_y_continuous(breaks=c(0.1, 0.2)) +
+  ggtitle('All measurement depths') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = element_blank(),
+       y = 'Density')
+
+hist2 <- ggplot(filter(manual, Depth<=8), aes(Temp_corrected)) + 
+  geom_histogram(aes(y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(color='orangered', size=1, bw=1) +
+  geom_histogram(data=filter(automatic, Depth<=7), aes(x=Temp, y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(data=filter(automatic, Depth<=7), aes(x=Temp), color='steelblue1', size=1, bw=1) +
+  scale_y_continuous(breaks=c(0.1, 0.2)) +
+  ggtitle('Measurement Depths 0-8m') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = element_blank(),
+       y = 'Density')
+
+hist3 <- ggplot(filter(manual, Depth>8), aes(Temp_corrected)) + 
+  geom_histogram(aes(y=..density..), fill='orangered', alpha=0.5) + 
+  geom_density(color='orangered', size=1, bw=1) +
+  geom_histogram(data=filter(automatic, Depth>7), aes(x=Temp, y=..density..), fill='steelblue1', alpha=0.5) +
+  geom_density(data=filter(automatic, Depth>7), aes(x=Temp), color='steelblue1', size=1, bw=1) +
+  ggtitle('Measurement Depths 8-20m') +
+  theme(plot.title = element_text(size = 10)) +
+  labs(x = 'Temperature (°C)',
+       y = 'Density')
+
+grid.arrange(hist1, hist2, hist3)
+
+#-------------------#
+#  Run the t-tests  #
+#-------------------#
+
+# All depths
+t.test(manual$Temp_corrected, automatic$Temp)
+# >> Statistically significant difference in means.
+
+# 1-7m
+t.test(manual$Temp_corrected[manual$Depth <= 7], automatic$Temp[automatic$Depth <= 7])
+# >> Statistically significant difference in means.
+
+# 9-19m
+t.test(manual$Temp_corrected[manual$Depth > 7], automatic$Temp[automatic$Depth > 7])
+# >> Statistically significant difference in means.
+
+# Bayesian estimation for two groups
+fit_all <- ttestBF(manual$Temp_corrected, automatic$Temp, 
+                   posterior=T, iterations=10000)
+summary(fit_all)
+hdi(fit_all)
+p_rope(fit_all)
+plot(fit_all)
+
+# 1-7m
+fit_lo <- ttestBF(manual$Temp_corrected[manual$Depth <= 7], 
+                  automatic$Temp[automatic$Depth <= 7], 
+                  posterior=T, iterations=10000)
+summary(fit_lo)
+hdi(fit_lo)
+p_rope(fit_all)
+plot(fit_lo)
+
+# 9-19m
+fit_hi <- ttestBF(manual$Temp_corrected[manual$Depth > 7], 
+                  automatic$Temp[automatic$Depth > 7], 
+                  posterior=T, iterations=10000)
+summary(fit_hi)
+hdi(fit_hi)
+p_rope(fit_hi)
+plot(fit_hi)
+
+# Jensen-Shannon divergence
+hist_manual_all = hist(manual$Temp_corrected, breaks=bins, plot=F)
+hist_automatic_all = hist(automatic$Temp, breaks=bins, plot=F)
+JSD(rbind(hist_manual_all$density, hist_automatic_all$density))
+
+hist_automatic_lo = hist(automatic$Temp[automatic$Depth<=7], breaks=bins, plot=F)
+hist_manual_lo = hist(manual$Temp_corrected[manual$Depth<=7], breaks=bins, plot=F)
+JSD(rbind(hist_manual_lo$density, hist_automatic_lo$density))
+
+hist_manual_hi = hist(manual$Temp_corrected[manual$Depth>7], breaks=bins, plot=F)
+hist_automatic_hi = hist(automatic$Temp[automatic$Depth>7], breaks=bins, plot=F)
+JSD(rbind(hist_manual_hi$density, hist_automatic_hi$density))
 
 # ##########################
 # # Group Proportion Plot  #
@@ -280,3 +585,4 @@ t.test(manual$Temp[manual$Depth > 7], automatic$Temp[automatic$Depth > 7])
 #   scale_y_continuous(breaks = c(0,5,10)) +
 #   labs(x = 'Depth (m)', color = 'Data', fill = 'Data',
 #        y = 'Proportion (%)')
+
